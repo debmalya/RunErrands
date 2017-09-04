846dfa48dba0f47577ca81e5768d12568e4ca4cc
Debmalya Jash <debmalya.jash@gmail.com>
2017-09-04 16:17:07 +0800

    Update Task11.sh

--- Task11.sh
+++ Task11.sh
@@ -5,6 +5,7 @@ location=$2/`date +%Y%m%d`
 echo ${location}
 mkdir -p ${location}
 count=0
+separator="-"
 
 # list all csv files in reverse chronological order with one column output
 # latest file will come first. That file we have to preserve.
@@ -13,7 +14,6 @@ for file in `ls -1r  $1/*csv`
 do
   # to get the file name pattern. File name will follow naming convention <pattern>-<timestamp>-<sequence_number>.csv.
   # "-" marks begining of the timestamp
-  separator="-"
   current_pattern=`echo ${file/$separator*/$separator}`
   if [ "$current_pattern" == "$previous_pattern" ]
   then
@@ -28,4 +28,24 @@ if [ $count -gt 0 ]
 then
     tar -cvzf ${location}.tar.gz ${location} >> /dev/null
 fi
-rm -rf ${location}
+
+# Remove files which are older than 10 days
+# Remove files which are older than 10 days
+for file in `ls -1v ${location}`
+do
+    # remove .tar.gz from the file name get YYYYMMDD format date
+    file_prefix=`echo $file | sed -e "s/.tar.gz//g"`
+    file_date=`date -d "${file_prefix}" +%s`
+
+    # find out the difference in day
+    days_between=`echo $(( ($today - $file_date) / 86400 ))`
+    if [ $days_between -gt 9 ]
+    then
+         echo "deleting..."
+         echo "${file}"
+         rm ${location}/${file}
+    else
+         break
+    fi
+done
+
