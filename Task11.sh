a6d8a2eb40713e4474520a26ce10e1e1ebc812fd
Debmalya Jash <debmalya.jash@gmail.com>
2017-09-04 12:15:30 +0800

    Update Task11.sh

--- Task11.sh
+++ Task11.sh
@@ -4,6 +4,7 @@ previous_pattern=""
 location=$2/`date +%Y%m%d`
 echo ${location}
 mkdir -p ${location}
+count=0
 
 # list all csv files in reverse chronological order with one column output
 # latest file will come first. That file we have to preserve.
@@ -18,9 +19,13 @@ do
   then
       # move the file to temporary location
       mv $file $location
+      $count=$count+1
   fi
   previous_pattern=$current_pattern
 done
 
-tar -cvzf ${location}.tar.gz ${location} >> /dev/null
+if [ $count -gt 0 ]
+then
+    tar -cvzf ${location}.tar.gz ${location} >> /dev/null
+fi
 rm -rf ${location}
