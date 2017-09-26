2fb04ae7f43998052ca101acd724eadadcf5d256
Debmalya Jash <debmalya.jash@gmail.com>
2017-09-26 10:38:33 +0800

    Update Task8.sh

--- Task8.sh
+++ Task8.sh
@@ -5,5 +5,6 @@
 # from this input file we have to split files based on passed number of lines in each file, parameter 1
 # First, store MSISDN in temporary file
 awk -F"|" '{print $1}' > ONLY_MSISDN.txt
-split -d --numeric_suffixes --lines=$1 ONLY_MSISDN.txt
+split -d --numeric_suffixes --lines=$2 ONLY_MSISDN.txt
+rm ONLY_MSISDN.txt
 # Linux singtel-dev 3.10.0-693.el7.x86_64 #1 SMP Thu Jul 6 19:56:57 EDT 2017 x86_64 x86_64 x86_64 GNU/Linux
