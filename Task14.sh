e752da9020da481f291960e6a2a06a7cd021f329
Debmalya Jash <debmalya.jash@gmail.com>
2017-09-26 15:49:37 +0800

    Update Task14.sh

--- Task14.sh
+++ Task14.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 first_file=0
-# Parameter 1: name of the final file (after merging)
+# Parameter 1: name of the merged_file
+
+
+if [ $# -ne 1 ]
+then
+   echo "missing required number of parameters."
+   exit 1
+fi
+
+if [ -e $1 ]
+then
+   rm $1
+fi
+
+
 for file in *
 do
+  echo $file
   if [ $first_file -eq 0 ]
   then
+     touch $1
      cat $file >$1
      first_file=1
   else
-     sed 1d $file >>$1
+     #sed 1d $file >>$1
+     cat $file >>$1
   fi
 done
