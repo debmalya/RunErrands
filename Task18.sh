d3990d697b948ebd934752d12700b34de6d92022
Debmalya Jash <debmalya.jash@gmail.com>
2019-05-20 10:43:56 +0800

    Get only header

--- Task18.sh
+++ Task18.sh
@@ -3,7 +3,9 @@ web_services=("http://localhost:8080/api/football/comp/PL/38")
 
 for i in "${web_services[@]}"
 do
-  if [ $(curl -s -o /dev/null -I -w "%{http_code}\n" $i) ==  "200" ] ;
+  # The parameter -I might be added to improve response load performance. 
+  # This parameter just request for status/headers of response, without download response body. (%{http_code} returns on first line of HTTP payload).
+  if [ $(curl -s -o /dev/null -i -I -w "%{http_code}\n" $i) ==  "200" ] ;
   then
       echo "$i - OK"
   else
