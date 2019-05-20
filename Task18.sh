427bf77fb4fae5dc62fc1bcfecdbcba49b5b207a
Debmalya Jash <debmalya.jash@gmail.com>
2019-05-20 21:00:14 +0800

    Adding response time to access log

--- Task18.sh
+++ Task18.sh
@@ -7,13 +7,14 @@ do
   # This parameter just request for status/headers of response, without download response body. (%{http_code} returns on first line of HTTP payload).
   epoch_time=`date +%s`
   status=$(curl -s -o /dev/null -i -I -w "%{http_code}\n" $i)
-  echo $status
+  end_time=`date +%s`
+  response_time=`expr $end_time - $epoch_time`
   if [ $status ==  "200" ] ;
   then
       echo "$i - OK"
-      echo "WEBSERVICE_MONITOR|$i|$epoch_time|$status|SUCCESS|" >> webservice_access.log
+      echo "WEBSERVICE_MONITOR|$i|$epoch_time|$status|SUCCESS||$response_time" >> webservice_access.log
   else
-      echo "WEBSERVICE_MONITOR|$i|$epoch_time|$status|FAILURE|" >> webservice_access.log
+      echo "WEBSERVICE_MONITOR|$i|$epoch_time|$status|FAILURE||$response_time" >> webservice_access.log
       echo "$i - KO"
   fi
 done 
