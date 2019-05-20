4e2bc18c7bb2604f2b896e355c49852a670b274f
Debmalya Jash <debmalya.jash@gmail.com>
2019-05-20 10:31:30 +0800

    Added -I to improve performance

--- Task18.sh
+++ Task18.sh
@@ -3,7 +3,7 @@ web_services=("http://localhost:8080/api/football/comp/PL/38")
 
 for i in "${web_services[@]}"
 do
-  if [ $(curl -s -o /dev/null -w "%{http_code}\n" $i) ==  "200" ] ;
+  if [ $(curl -s -o /dev/null -I -w "%{http_code}\n" $i) ==  "200" ] ;
   then
       echo "$i - OK"
   else
